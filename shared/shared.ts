30543713354bcbc16c9c8fd4281e7f83cbfd7deb
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-04-03 02:26:52 -0700

    create `renderElement`

---
+++ shared/shared.ts
@@ -0,0 +1,59 @@
+/**
+ * renderElement renders an HTML element to a string.
+ */
+export function renderElement<T>(
+  tag: string,
+  props: T & { children?: unknown[] },
+  isVoidElement = false,
+): string {
+  const renderedAttrs = renderAttrs(props);
+  const openingTag = `<${tag}${renderedAttrs ? ` ${renderedAttrs}` : ""}>`;
+  if (isVoidElement) {
+    return openingTag;
+  }
+
+  return `${openingTag}${(props.children ?? []).join("")}</${tag}>`;
+}
+
+function renderAttrs<T>(props: T): string {
+  let attrs = "";
+  for (const key in props) {
+    if (key === "children") {
+      continue;
+    }
+
+    const value = props[key];
+    switch (typeof value) {
+      case "string":
+      case "number": {
+        attrs += `${key}="${value}" `;
+        break;
+      }
+
+      case "boolean": {
+        if (value) {
+          attrs += `${key} `;
+        }
+
+        break;
+      }
+
+      case "function": {
+        if (value.name === "") {
+          continue;
+        }
+
+        attrs += `${key}="${value.name}"`;
+        break;
+      }
+
+      default: {
+        throw new Error(
+          `Invalid prop type ${typeof value} for prop ${key}`,
+        );
+      }
+    }
+  }
+
+  return attrs;
+}
